f0899c54a3f27ea5d0a164962dda3171615f3cf1
Sarah <sarahjreive@gmail.com>
2019-07-03 17:02:32 +0000

    moved connect module into play.js

--- play.js
+++ play.js
@@ -1,21 +1,7 @@
+const { connect } = require('./client')
 const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '172.46.0.110',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  conn.on('data',(data) => {
-    console.log('Message from server:', data);
-});
 
-  return conn;
-}
 
 console.log('Connecting ...');
 connect();
