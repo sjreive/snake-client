41ddb8f3807a7a0b4ed7cb4b1c88f96c50046e65
Sarah <sarahjreive@gmail.com>
2019-07-03 18:32:02 +0000

    created module for handling user input

---
+++ input.js
@@ -0,0 +1,18 @@
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput(stdin); 
+  return stdin;   
+};
+
+const handleUserInput = function (stdin) {
+  stdin.on('data', (key) => {
+    if (key === "\u0003") {
+      process.exit();
+    }
+  })
+};
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,7 +1,14 @@
 const { connect } = require('./client')
+const { setupInput } =require('./input');
 const net = require('net');
 
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
 
 
 console.log('Connecting ...');
 connect();
+setupInput();
+
